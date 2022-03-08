7c84d571269fc7b184982c8c680fea9b595efc07
chogyejin <whrpwls96@naver.com>
2022-03-08 19:25:43 +0900

    feat: Add sign-out button in Profile.tsx

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,8 @@
-import { User } from "firebase/auth";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 interface AppRouterProps {
   isLoggedIn: boolean;
@@ -10,10 +11,12 @@ interface AppRouterProps {
 const AppRouter = ({ isLoggedIn }: AppRouterProps) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
           </>
         ) : (
           <Route path="/" element={<Auth />} />
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,5 +1,18 @@
+import { authService } from "../fbase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 const Profile = () => {
-  return <span>Profile</span>;
+  const navigate = useNavigate();
+  const onLogOut = () => {
+    signOut(authService);
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOut}>로그아웃</button>
+    </>
+  );
 };
 
 export default Profile;
